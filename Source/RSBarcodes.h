268afa65d2124d610800025776a192ec82c07c3d
Markus Kerschbaum <markus.kerschbaum@rise-world.com>
2017-06-01 14:27:17 +0200

    fixes compile warnings "implicit import of bridging header 'RSBarcodes-Bridging-Header.h' via module 'RSBarcodes' is deprecated and will be removed in a later version of Swift"

---
+++ Source/RSBarcodes.h
@@ -0,0 +1,19 @@
+//
+//  Header.h
+//  RSBarcodes
+//
+//  Created by Markus Kerschbaum on 01.06.17.
+//  Copyright © 2017 P.D.Q. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AwesomeCache.
+FOUNDATION_EXPORT double RSBarcodesVersionNumber;
+
+//! Project version string for AwesomeCache.
+FOUNDATION_EXPORT const unsigned char RSBarcodesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import
+
+#import "ContextMaker.h"
